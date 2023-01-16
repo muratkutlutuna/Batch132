c12ba68c7876bd8f10f697545a78910eb795328b
muratkutlutuna <muratkutlutuna@gmail.com>
2023-01-16 21:53:49 +0100

    day10

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+^
+.src/main/java/day08ternaryswitch/Switch03.java,3/9/39bf1703fadb59deb2ff37f8714c520aba4d79b9
---
+++ src/main/java/day10forloopwhileloop/NestedForLoop01.java
@@ -0,0 +1,54 @@
+package day10forloopwhileloop;
+
+public class NestedForLoop01 {
+    public static void main(String[] args) {
+
+        /*
+            2.Example:Type code to get the output like
+                Week: 1
+                  Day: 1
+                  Day: 2
+                  Day: 3
+                  .....
+                Week: 2
+                  Day: 1
+                  Day: 2
+                  Day: 3
+                  ....
+                Week: 3
+                  Day: 1
+                  Day: 2
+                  Day: 3
+                  ....
+ */
+        /*
+        Nested loops are "Time consuming", that is why if it is not mandatory, do not prefer to use "Nested Loops"
+        If a code is "time-consuming" it means it has "time complexity"
+        There is a way to calculate the value of "Time complexity": if you have 2 loops, time complexity is the square of 2 ==> 4
+                                                                    if you have 2 loops, time complexity is the cube of 3 ==> 27
+         */
+        for (int i = 1; i < 5; i++) {
+            System.out.println("Week; " + i);
+            for (int j = 1; j <= 7; j++) {
+                System.out.println("\tday: " + j);
+            }
+        }
+
+        /*
+     Example 3: Write a Java Program to Print Rectangle Star Pattern using For Loop
+                 ****
+                 ****
+                 ****
+     Note: Get the number of rows and columns from user
+ */
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 4; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
